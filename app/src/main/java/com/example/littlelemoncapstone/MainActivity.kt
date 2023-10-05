1cf464dde8fabe93825584f87edd2cc6833d635b
MLSALEX <stiopovna@mail.ru>
2023-10-05 19:44:54 +0300

    menu

--- app/src/main/java/com/example/littlelemoncapstone/MainActivity.kt
+++ app/src/main/java/com/example/littlelemoncapstone/MainActivity.kt
@@ -6,14 +6,45 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.example.littlelemoncapstone.ui.theme.LittleLemonCapstoneTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "menuDatabase").build()
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        return httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json ")
+            .body<MenuNetwork>()
+            .menu
+    }
+    private fun saveMenuToDatabase(menuItemNetwork: List<MenuItemNetwork>) {
+        val menuItemRoom = menuItemNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemRoom.toTypedArray())
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,6 +56,12 @@ class MainActivity : ComponentActivity() {
                 ) {
                     MyNavigation()
                 }
+            }
+        }
+        lifecycleScope.launch(Dispatchers.IO) {
+            if (database.menuItemDao().isEmpty()) {
+                val menuItemNetwork = fetchMenu()
+                saveMenuToDatabase(menuItemNetwork)
             }
         }
     }
